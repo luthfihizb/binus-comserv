0306df008f6b5a3b29760b1cf8cb6f4469f35647
Luthfi Hizb <luthfihizb@gmail.com>
2024-04-19 23:59:27 +0700

    feat: validation & form handling

--- form/index.php
+++ form/index.php
@@ -7,10 +7,9 @@
         <h1>Kamu suka kucing apa?</h1>
 
         <form action="result.php" method="POST"> 
-            <input placeholder="Masukkan nama" name="kucing" />
-            <input placeholder="Masukkan URL gambar" name="gambar" />
+            <input placeholder="Masukkan nama" name="kucing" required />
+            <input placeholder="Masukkan URL gambar" name="gambar" required />
             <button>Simpan</button>
         </form>
     </body>
 </html>
-
--- form/result.php
+++ form/result.php
@@ -5,10 +5,24 @@
     </head>
     <body>
         <h3>Kucing kesukaanku</h3>
-        
-        <p>
-            <?php echo $_POST['kucing']; ?> <br />
-        </p>
+
+        <?php
+
+            // Condition
+            // Kalo input kucing isinya "Kucing Oren",
+            // Kasih pesan "Itu kucing kesukaan aku!!!"
+            // Tapi kalo selain itu
+            // Keluarin data normal (nama + gambar)
+
+            if ($_POST['kucing'] === "kucing oren"):
+        ?>
+            Itu kucing kesukaan aku!!!"
+        <?php else: ?>
+            <p>
+                <?php echo $_POST['kucing']; ?> <br />
+            </p>
+            <img src="<?php echo $_POST['gambar']; ?>"/>
+        <?php endif; ?>
 
         <a href="/binus-comserv/form/">Kembali</a>
     </body>
